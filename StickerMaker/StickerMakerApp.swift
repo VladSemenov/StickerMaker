16453e7444167ecbbb338912a161484ac40d443f
Vladislav Semenov <siamionau.ulad@gmail.com>
2025-10-25 00:33:16 +0200

    Initial Commit

---
+++ StickerMaker/StickerMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerMakerApp.swift
+//  StickerMaker
+//
+//  Created by Vladislav Semenov on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StickerMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
